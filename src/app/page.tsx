b2d3de5c10e867cdb3e17866a52076b27a593a64
Pratik Khaire <pratikkhaire2004@gmail.com>
2025-07-19 21:32:43 +0530

    morden landing page design

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+import { Container } from "@/components/container";
+import Hero from "@/components/hero";
+import Navbar from "@/components/navbar";
+
+export default function Home() {
+  return (
+    <>
+      <div className="layout">
+        <Container>
+          <Navbar/>
+          <Hero/>
+        </Container>
+      </div>
+    </>
+  );
+}
---
+++ src/components/hero.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const hero = () => {
+    return (
+        <div>
+            <div className="badge">
+                <span>
+                    Thinking about incorporating?
+                </span>
+                <svg width="16" height="16" fill="none">
+                    <path stroke="#1E1F25" stroke-linecap="round" stroke-linejoin="round" stroke-opacity=".5" stroke-width="1.25" d="M8 4.75 11.25 8m0 0L8 11.25M11.25 8h-6.5"></path>
+                    </svg>
+            </div>
+        </div>
+    )
+}
+
+export default hero
